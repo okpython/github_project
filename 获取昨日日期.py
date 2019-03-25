dfe09fb81b63ea84b8a15935ccf09d44365d4700
itpython <wrb199111@163.com>
2019-03-25 15:04:29 +0800

    获取昨日日期，以及str和datetime.date格式直接转换

---
+++ 获取昨日日期.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+#coding:utf-8
+
+import datetime
+
+def getYesterday():
+    today = datetime.date.today()
+    oneday = datetime.timedelta(days=1)
+    yesterday = today-oneday
+    return yesterday
+
+yesterday = getYesterday()
+"""
+2019-03-24
+<type 'datetime.date'>
+"""
+print(yesterday)
+print(type(yesterday))
+# 将datetime.date转为str
+yesterday = datetime.datetime.strftime(yesterday, "%Y-%m-%d")
+"""
+2019-03-24
+<type 'str'>
+"""
+print(yesterday)
+print(type(yesterday))
+# 将str转为datetime.date
+yesterday=datetime.datetime.strptime(yesterday, "%Y-%m-%d")
+"""
+2019-03-24 00:00:00
+<type 'datetime.datetime'>
+"""
+print(yesterday)
+print(type(yesterday))
